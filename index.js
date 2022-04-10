e8f6747f81ddb2c1a7da2e00bc6a3888131e1b1a
Michael Warren <mwarrn@amazon.com>
2022-04-10 15:28:22 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,37 +1,99 @@
-function forEach() {
-
+function forEach(array, callback) {
+  for (let i = 0; i < array.length; i++) {
+    callback(array[i], i, array);
+  }
 }
 
-function map() {
-
+function map(array, callback) {
+  const newArray = [];
+  for (element of array) {
+    newArray.push(callback(element));
+  }
+  return newArray;
 }
 
-function filter() {
-
+function filter(array, callback) {
+  const newArray = [];
+  for (element of array) {
+    if (callback(element) === true) {
+      newArray.push(element);
+    }
+  }
+  return newArray;
 }
 
-function every() {
-
+function every(array, callback) {
+  for (element of array) {
+    if (callback(element) === false) {
+      return false;
+    }
+  }
+  return true;
 }
 
-function some() {
-
+function some(array, callback) {
+  for (element of array) {
+    if (callback(element) === true) {
+      return true;
+    }
+  }
+  return false;
 }
 
-function majority() {
+function majority(array, callback) {
+  const maj = Math.floor((array.length / 2) + 1);
+  const newArray = [];
 
+  for (element of array) {
+    if (callback(element) === true) {
+      newArray.push(element);
+    }
+  }
+  if (newArray.length >= maj) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
-function once() {
+function once(callback) {
+  let isCalled = false;
+  let value;
 
+  return function(args) {
+    if (isCalled) {
+      return value;
+    }
+
+    isCalled = true;
+    value = callback(args);
+    return value;
+  }
 }
 
-function groupBy() {
+function groupBy(array, callback) {
+  const object = {};
 
+  for (element of array) {
+    let key = callback(element);
+
+    if (object[key]) {
+      object[key].push(element);
+    } else {
+      object[key] = [element];
+    }
+  }
+  return object;
 }
 
-function arrayToObject() {
+function arrayToObject(array, callback) {
+  let object = {};
 
+  for (element of array) {
+    object[element] = callback(element)
+  }
+  return object;
 }
 
 module.exports = { 
